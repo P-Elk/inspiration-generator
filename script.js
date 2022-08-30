d59f652c49e97bde74eca7d0b81fc56a710f4f44
P-Elk <Elkington@users.noreply.github.com>
2022-08-30 11:51:21 +0200

    Added generator

---
+++ script.js
@@ -0,0 +1,68 @@
+"use strict";
+
+const motivation = [
+  "When you have a dream, you've got to grab it and never let go.",
+  "Nothing is impossible. The word itself says, I'm possible!",
+  "There is nothing impossible to they who will try",
+  "The bad news is time flies. The good news is you're the pilot.",
+  "Life has got all those twists and turns. You've got to hold on tight and off you go.",
+  "Keep your face always toward the sunshine, and shadows will fall behind you.",
+  "Be courageous. Challenge orthodoxy. Stand up for what you believe in. When you are in your rocking chair talking to your grandchildren many years from now, be sure you have a good story to tell.",
+  "You make a choice: continue living your life feeling muddled in this abyss of self-misunderstanding, or you find your identity independent of it. You draw your own box.",
+  "I just want you to know that if you are out there and you are being really hard on yourself right now for something that has happened ... it's normal. That is what is going to happen to you in life. No one gets through unscathed. We are all going to have a few scratches on us. Please be kind to yourselves and stand up for yourself, please.",
+  "Success is not final, failure is not fatal: it is the courage to continue that counts.",
+  "You define your own life. Don't let other people write your script.",
+  "You are never too old to set another goal or to dream a new dream.",
+  "At the end of the day, whether or not those people are comfortable with how you're living your life doesn't matter. What matters is whether you're comfortable with it.",
+  "People tell you the world looks a certain way. Parents tell you how to think. Schools tell you how to think. TV. Religion. And then at a certain point, if you're lucky, you realize you can make up your own mind. Nobody sets the rules but you. You can design your own life.",
+  "For me, becoming isn't about arriving somewhere or achieving a certain aim. I see it instead as forward motion, a means of evolving, a way to reach continuously toward a better self. The journey doesn't end.",
+  "Spread love everywhere you go.",
+  "Do not allow people to dim your shine because they are blinded. Tell them to put some sunglasses on.",
+  "If you make your internal life a priority, then everything else you need on the outside will be given to you and it will be extremely clear what the next step is.",
+  "You don't always need a plan. Sometimes you just need to breathe, trust, let go and see what happens.",
+  "You can be everything. You can be the infinite amount of things that people are.",
+];
+
+const joke = [
+  "My wife told me to stop impersonating a flamingo. I had to put my foot down.",
+
+  "I went to buy some camo pants but couldn't find any.",
+
+  "I failed math so many times at school, I can't even count.",
+
+  "I used to have a handle on life, but then it broke.",
+
+  "I was wondering why the frisbee kept getting bigger and bigger, but then it hit me.",
+
+  "I heard there were a bunch of break-ins over at the car park. That is wrong on so many levels.",
+
+  "I want to die peacefully in my sleep, like my grandfather… Not screaming and yelling like the passengers in his car.",
+
+  "When life gives you melons, you might be dyslexic.",
+
+  "Don't you hate it when someone answers their own questions? I do.",
+
+  "It takes a lot of balls to golf the way I do.",
+  "I told him to be himself; that was pretty mean, I guess.",
+
+  "I know they say that money talks, but all mine says is Goodbye.",
+
+  "My father has schizophrenia, but he's good people.",
+
+  "The problem with kleptomaniacs is that they always take things literally.",
+
+  "I can't believe I got fired from the calendar factory. All I did was take a day off.",
+
+  "Most people are shocked when they find out how bad I am as an electrician.",
+
+  "Never trust atoms; they make up everything.",
+
+  "My wife just found out I replaced our bed with a trampoline. She hit the ceiling!",
+
+  "I was addicted to the hokey pokey, but then I turned myself around.",
+
+  "I used to think I was indecisive. But now I'm not so sure.",
+];
+
+console.log(motivation.length);
+console.log(joke.length);
